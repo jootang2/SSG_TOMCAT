8aacb028c064e81473c1e531a4689c47b3b3ddd3
jootang2 <joohwan.song15b@gmail.com>
2022-07-24 23:17:34 +0900

    게시물 객체 3개 노출

--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -1,9 +1,23 @@
 package com.ll.exam.article;
 
 import com.ll.exam.Rq;
+import com.ll.exam.article.dto.ArticleDto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArticleController {
     public void showList(Rq rq){
-        rq.appendBody("게시물 리스트");
+        List<ArticleDto> articleDtos = new ArrayList<>();
+        articleDtos.add(new ArticleDto(5,"제목 5", "내용5"));
+        articleDtos.add(new ArticleDto(4,"제목 4", "내용4"));
+        articleDtos.add(new ArticleDto(3,"제목 3", "내용3"));
+        articleDtos.add(new ArticleDto(2,"제목 2", "내용2"));
+        articleDtos.add(new ArticleDto(1,"제목 1", "내용1"));
+
+        rq.setAttr("articles", articleDtos);
+
+
+        rq.view("usr/article/list");
     }
 }
